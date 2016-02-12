1b1396e2923afef381d551ae3cedeb8ff709640f
George Dewar <george@dewar.co.nz>
2016-02-12 23:41:34 +1300

    Throw in the month for good measure

--- app/assets/javascripts/discovery.js
+++ app/assets/javascripts/discovery.js
@@ -2,53 +2,80 @@ var ndx;
 var priceByTimeDimension;
 var priceByTimeGroup;
 
+var avgAdd = function (p, v) {
+  ++p.count;
+  p.total += v.Price;
+  p.avg = p.total / p.count;
+  return p;
+}
+
+var avgRemove = function (p, v) {
+  --p.count;
+  p.total -= v.Price;
+  p.avg = p.count ? p.total / p.count : 0;
+  return p;
+}
+
+var avgInitial = function () {
+  return {count: 0, total: 0, avg: 0};
+}
+
 $(function(){
   d3.csv("/TKR0331.csv", function(data) {
     data.forEach(function(d){
-      d.Trading_period = +d.Trading_period;
+      d.Trading_period = (+d.Trading_period - 1) / 2;
       d.Price = +d.Price;
+      d.Trading_date = new Date(d.Trading_date);
     });
+
+    console.log(data)
 
     ndx = crossfilter(data);
 
     priceByTimeDimension = ndx.dimension(function(d) {
       return d.Trading_period;
     });
-    priceByTimeGroup = priceByTimeDimension.group().reduce(
-      function (p, v) {
-        ++p.count;
-        p.total += v.Price;
-        p.avg = p.total / p.count;
-        return p;
-      },
-      function (p, v) {
-        --p.count;
-        p.total -= v.Price;
-        p.avg = p.count ? p.total / p.count : 0;
-        return p;
-      },
-      function () {
-        return {count: 0, total: 0, avg: 0};
-      }
-    );
+    priceByTimeGroup = priceByTimeDimension.group().reduce(avgAdd, avgRemove, avgInitial);
+
+    priceByDomDimension = ndx.dimension(function(d) {
+      return d.Trading_date;
+    });
+    priceByDomGroup = priceByDomDimension.group().reduce(avgAdd, avgRemove, avgInitial);
 
     var priceByTimeChart = dc.lineChart('#price_by_time_chart')
       .width(768)
       .height(480)
-      .x(d3.scale.linear().domain([0,48]))
+      .x(d3.scale.linear().domain([0,23.5]))
       .renderArea(true)
       .brushOn(true)
       .yAxisLabel("Price ($/GWh)")
-      //.elasticY(true)
+      .elasticY(true)
       .dimension(priceByTimeDimension)
       .group(priceByTimeGroup)
       .valueAccessor(function(d){
         return d.value.avg;
       });
+    priceByTimeChart.width($('#price_by_time_chart').width())
 
-    priceByTimeChart.render();
+    var priceByDomChart = dc.lineChart('#price_by_dom_chart')
+      .width(768)
+      .height(200)
+      .x(d3.time.scale().domain([new Date(2016,0,1), new Date(2016,1,0)]))
+      .round(d3.time.day.round)
+      .renderArea(true)
+      .brushOn(true)
+      .yAxisLabel("Price ($/GWh)")
+      //.elasticY(true)
+      .dimension(priceByDomDimension)
+      .group(priceByDomGroup)
+      .valueAccessor(function(d){
+        return d.value.avg;
+      });
+    priceByDomChart.width($('#price_by_dom_chart').width())
+
+
     dc.renderAll();
-    dc.redrawAll();
+
 
   });
 });
