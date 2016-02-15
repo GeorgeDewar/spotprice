c092bc7989c35dba41f515342b9fa5d84ce296d0
George Dewar <george@dewar.co.nz>
2016-02-15 21:20:36 +1300

    Don't check in price data (script can fetch it)

--- scripts/fetch_prices.sh
+++ scripts/fetch_prices.sh
@@ -2,4 +2,4 @@
 
 month=$1
 
-curl "http://www.emi.ea.govt.nz/Datasets/download?directory=%2FDatasets%2FWholesale%2FFinal_pricing%2FFinal_prices%2F${month}_Final_prices.csv" > "data/prices_${month}.csv"
+curl "http://www.emi.ea.govt.nz/Datasets/download?directory=%2FDatasets%2FWholesale%2FFinal_pricing%2FFinal_prices%2F${month}_Final_prices.csv" > "tmp/prices_${month}.csv"
