deb139ebdd5df1a4a8d0f96816f240d01d212f2e
George Dewar <george@dewar.co.nz>
2016-02-16 09:56:33 +1300

    Cache results

--- app/controllers/discovery_controller.rb
+++ app/controllers/discovery_controller.rb
@@ -14,15 +14,19 @@ class DiscoveryController < ApplicationController
 
   def data
     node = Node.find_by_code!(params[:node])
-    prices = ActiveRecord::Base.connection.select_all <<-SQL
-    select date, period, price from prices where node_id = #{node.id} and date >= '#{Price.maximum(:date) - 12.months + 1.day}'
-    SQL
-    render body: (CSV.generate(encoding: "UTF-8") do |csv|
-      csv << prices.first.keys
-      prices.each do |x|
-        csv << x.values
+    max_date = Price.maximum(:date)
+    csv = Rails.cache.fetch("prices/#{node.code}/#{max_date}/csv") do
+      prices = ActiveRecord::Base.connection.select_all <<-SQL
+      select date, period, price from prices where node_id = #{node.id} and date >= '#{max_date - 12.months + 1.day}'
+      SQL
+      CSV.generate(encoding: "UTF-8") do |csv|
+        csv << prices.first.keys
+        prices.each do |x|
+          csv << x.values
+        end
       end
-    end)
+    end
+    render body: csv
   end
 
   def about
