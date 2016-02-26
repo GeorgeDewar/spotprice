644af31aec9cd40cd38a1e360a24fbaad96d8311
George Dewar <george@dewar.co.nz>
2016-02-26 19:38:41 +1300

    Annotate

--- app/models/generation_amount.rb
+++ app/models/generation_amount.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: generation_amounts
+#
+#  id           :integer          not null, primary key
+#  generator_id :integer          not null
+#  node_id      :integer          not null
+#  network_code :string           not null
+#  date         :date             not null
+#  period       :integer          not null
+#  quantity     :decimal(, )      not null
+#
+
 class GenerationAmount < ActiveRecord::Base
   belongs_to :generator
 end
--- app/models/generator.rb
+++ app/models/generator.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: generators
+#
+#  id         :integer          not null, primary key
+#  code       :string           not null
+#  name       :string
+#  fuel       :string           not null
+#  technology :string           not null
+#
+
 class Generator < ActiveRecord::Base
   belongs_to :node
   has_many :generation_amounts
