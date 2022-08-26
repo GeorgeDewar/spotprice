4085b94a38c0553aaa39a35e0533c91b8ba979fe
George Dewar <george@dewar.co.nz>
2022-08-26 22:46:10 +1200

    Fix bug, fetch months AND days as required

--- app/models/price.rb
+++ app/models/price.rb
@@ -12,15 +12,32 @@
 class Price < ActiveRecord::Base
   belongs_to :node
 
-  # Fetch a day's worth of prices from the EMI website (e.g. 20220823)
-  def self.fetch_day(date)
-    url = "https://www.emi.ea.govt.nz/Wholesale/Datasets/FinalPricing/EnergyPrices/#{date}_FinalEnergyPrices.csv"
+  # Fetch a month's worth of prices from the EMI website (e.g. 202208)
+  def self.fetch_month(month_code)
+    url = "https://www.emi.ea.govt.nz/Wholesale/Datasets/FinalPricing/EnergyPrices/ByMonth/#{month_code}_FinalEnergyPrices.csv"
     response = HTTParty.get url
     if response.code != 200
-      puts "Error #{response.code} from server while fetching prices for #{date} from #{url}"
+      puts "Error #{response.code} from server while fetching prices for #{month_code} from #{url}"
       return
     end
 
+    process_data(response.body)
+  end
+
+  # Fetch a day's worth of prices from the EMI website (e.g. 20220823)
+  def self.fetch_day(day_code)
+    url = "https://www.emi.ea.govt.nz/Wholesale/Datasets/FinalPricing/EnergyPrices/#{day_code}_FinalEnergyPrices.csv"
+    response = HTTParty.get url
+    if response.code != 200
+      puts "Error #{response.code} from server while fetching prices for #{day_code} from #{url}"
+      return
+    end
+
+    process_data(response.body)
+  end
+
+
+  def self.process_data(csv_body)
     i = 0
     batch = []
     batch_size = 1000
@@ -28,7 +45,7 @@ class Price < ActiveRecord::Base
     nodes = Node.all.map{ |n| { n.code => n.id }}.reduce(:merge) || {}
 
     Price.transaction do
-      CSV.parse(response.body, headers: true) do |line|
+      CSV.parse(csv_body, headers: true) do |line|
         i += 1
         puts i if i % 1000 == 0
         node_id = nodes[line["PointOfConnection"]]
--- scripts/load_data.rb
+++ scripts/load_data.rb
@@ -9,16 +9,24 @@ require 'csv'
 # at the beginning of next month, allowing a whole month for it to be uploaded.
 
 first_month = Date.today - 13.months
-months = 12.times.map { |x| (first_month + x.months) }
+months = 14.times.map { |x| (first_month + x.months) }
 
 months.each do |month|
   month_code = month.strftime("%Y%m")
+  puts "Checking #{month_code}"
 
+  # Try the whole month first
+  unless Price.where(date: month).exists?
+    puts "Fetching prices for month: #{month_code}..."
+    Price.fetch_month month_code
+  end
+
+  # Then try each individual day
   beginning = month.beginning_of_month
   num_days_in_month = month.end_of_month.day
   num_days_in_month.times do |i|
-    unless Price.where(date: month).exists?
-      day = beginning + i
+    day = beginning + i
+    unless Price.where(date: day).exists?
       day_code = day.strftime("%Y%m%d")
       puts "Fetching prices for #{day_code}..."
       Price.fetch_day day_code
